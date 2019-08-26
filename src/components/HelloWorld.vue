c0ac85bcd66ce59bc7b18ac0a9225bfe8a467771
yel125 <1252708480@qq.com>
2019-08-26 21:44:14 +0800

    111

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
+        <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>    <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li>
